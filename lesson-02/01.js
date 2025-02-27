ac104900c1a40acc88a4f19f75e42a7b3394c965
Andrey <wolterius@gmail.com>
2025-02-27 11:35:48 +0300

    complete task 2-1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,15 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
+const isAdmin = true
+const isVerifiedUser = false
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess
+let isAccess = false
 
 // your code
+if((isAdmin === true || isVerifiedUser === true) && (hasSpecialPermission === true || hasTemporaryPass === true)){
+    isAccess = true;
+}
+console.log(isAccess)
