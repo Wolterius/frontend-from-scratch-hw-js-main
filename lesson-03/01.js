ce7c27b5cf43b29a9a00bec53c56d08e08ce2ae5
Andrey <wolterius@gmail.com>
2025-03-03 20:46:05 +0300

    complete task 3-1,2,3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage() {
+    let a = +prompt("Введите число a:")
+    let b = +prompt("Введите число b")
+    return (a+b)/2
+}
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,14 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest() {
+    let a = +prompt("Введите число a")
+    let b = +prompt("Введите число b")
+    let c = +prompt("Введите число c")
+    if(a > b && a > c){
+        return a
+    }else if(b > a && b > c){
+        return b
+    }else{
+        return c
+    }
+}
